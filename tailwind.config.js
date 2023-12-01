e6998af172ca1e0e4ee2c79e25e8cb3e549bad8d
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-12-01 18:38:20 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        // updated content
+        //  "./src/**/*.{html, jsx}",
+        // "./src/**/*",
     ],
     theme: {
         extend: {
